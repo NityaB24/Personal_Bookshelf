d37751ad70d04bb2f419bb54fb5ee40ab1c499ad
NityaB24 <nityabalar@gmail.com>
2024-06-04 12:02:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import BookSearchPage from './components/BookSearchPage.jsx';
+import BookshelfPage from './components/BookshelfPage.jsx';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<BookSearchPage />} />
+        <Route path="/bookshelf" element={<BookshelfPage />} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
